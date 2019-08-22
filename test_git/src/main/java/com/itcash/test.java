7da94ad24a9c4bb3cd5cc888dda9d2615244edca
hellocss <50293155+hellocsss@users.noreply.github.com>
2019-08-22 11:35:06 +0800

    Update test.java

--- test_git/src/main/java/com/itcash/test.java
+++ test_git/src/main/java/com/itcash/test.java
@@ -7,7 +7,6 @@ public class test {
         System.out.println("hello word");
         System.out.println("hello word");
         System.out.println("hello word");
-      //sssdsf
-        //ssss
+    
     }
 }
